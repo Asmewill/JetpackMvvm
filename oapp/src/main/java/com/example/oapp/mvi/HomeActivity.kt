0097e879b5b7308d6cbf0cf1bc438fb0977c940d
shuij <864805002@qq.com>
2025-08-15 18:58:14 +0800

    解决wapp下，RoomDatabase缺失之前数据库版本1.json导致无法运行项目问题.

--- oapp/src/main/java/com/example/oapp/mvi/HomeActivity.kt
+++ oapp/src/main/java/com/example/oapp/mvi/HomeActivity.kt
@@ -4,7 +4,6 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.oapp.R
-import com.example.oapp.databinding.ActivityHomeBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -12,14 +11,14 @@ import kotlinx.coroutines.launch
  * Created by Owen on 2025/4/19
  */
 class HomeActivity:BaseVMActivity<HomeViewModel>() {
-    lateinit var mDataBind:ActivityHomeBinding
+   // lateinit var mDataBind:ActivityHomeBinding
 
     override fun createViewModel(): HomeViewModel {
          return ViewModelProvider(this).get(HomeViewModel::class.java);
     }
 
     override fun initData() {
-        mDataBind = DataBindingUtil.setContentView(this, R.layout.activity_home) //实例化mDataBind
+       // mDataBind = DataBindingUtil.setContentView(this, R.layout.activity_home) //实例化mDataBind
         mViewModel.dispatchIntent(HomeIntent.GetHomeList) //请求第一页数据
        // mViewModel.dispatchIntent(HomeIntent.GetHomeListMore) //上拉加载下一页数据
         //添加观察者,处理数据
--- oapp/src/main/java/com/example/oapp/mvvm/HomeActivity.kt
+++ oapp/src/main/java/com/example/oapp/mvvm/HomeActivity.kt
@@ -3,24 +3,24 @@ package com.example.oapp.mvvm
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.oapp.R
-import com.example.oapp.databinding.ActivityHomeBinding
+
 
 /**
  * Created by Owen on 2025/4/19
  */
 class HomeActivity:BaseVMActivity<HomeViewModel>() {
-    lateinit var mDataBind:ActivityHomeBinding
+    //lateinit var mDataBind:ActivityHomeBinding
 
     override fun createViewModel(): HomeViewModel {
          return ViewModelProvider(this).get(HomeViewModel::class.java);
     }
 
     override fun initData() {
-        mDataBind = DataBindingUtil.setContentView(this, R.layout.activity_home) //实例化mDataBind
+      //  mDataBind = DataBindingUtil.setContentView(this, R.layout.activity_home) //实例化mDataBind
         mViewModel.getHomeList() //请求数据
         //添加观察者,处理数据
         mViewModel.homeLiveData.observe(this) { //处理数据
-            mDataBind.homeData=it;//数据双向绑定
+           // mDataBind.homeData=it;//数据双向绑定
        }
     }
 }
